bbd13a360ee5a4d6743892196d8efb6496926631
Nick Zheliba <nikolaj.jeliba@gmail.com>
2022-07-31 11:53:35 +0300

    add JS code for task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const itemsGalleryMarkup = createItemsGalleryMarkup(galleryItems);
+
+function createItemsGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    </div>`;
+    })
+    .join("");
+}
+
+galleryEl.insertAdjacentHTML("beforeend", itemsGalleryMarkup);
+
+galleryEl.addEventListener("click", onClickGallery);
+
+function onClickGallery(event) {
+  event.preventDefault();
+  console.log(event.target);
+}
+
+const addOptionsLightBox = {
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  heightRatio: 1,
+};
+
+let gallery = new SimpleLightbox(".gallery a", addOptionsLightBox);
+
+gallery.on("show.simplelightbox");
